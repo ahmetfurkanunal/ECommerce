ddc738b61d09abd8024d574890bbc45f18fea593
Ahmet Furkan Ünal <afurkanx33@gmail.com>
2025-03-03 20:54:49 +0300

    03.03

---
+++ ECommerce.Service/Abstract/ICartService.cs
@@ -0,0 +1,20 @@
+﻿using ECommerce.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Service.Abstract
+{
+    public interface ICartService
+    {
+        void AddProduct(Product product, int quantity);
+        void UpdateProduct(Product product, int quantity);
+        void RemoveProduct(Product product);
+
+        decimal TotalPrice();
+
+        void ClearAll();
+    }
+}
---
+++ ECommerce.WebUI/Models/CartViewModel.cs
@@ -0,0 +1,11 @@
+﻿using ECommerce.Core.Entities;
+
+namespace ECommerce.WebUI.Models
+{
+    public class CartViewModel
+    {
+        public List<CartLine>? CartLines { get; set; }
+
+        public decimal TotalPrice { get; set; }
+    }
+}
